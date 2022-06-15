576744e576d41d8fd3d1ba4c23ee2f9a828d9c32
Sophie Aubier <sophie.aubier@oslandia.com>
2022-06-15 10:36:03 +0200

    corrections

--- vectiler/gui/wzd_import.py
+++ vectiler/gui/wzd_import.py
@@ -6,7 +6,7 @@ import os
 # PyQGIS
 
 from qgis.PyQt import uic
-from PyQt5.QtWidgets import QWizard,QShortcut
+from PyQt5.QtWidgets import QWizard , QShortcut
 from PyQt5 import QtCore, QtGui
 
 # ############################################################################
@@ -16,26 +16,28 @@ from PyQt5 import QtCore, QtGui
 class WzdImport(QWizard):
 
     def __init__(self, parent=None):
-        super().__init__(parent)
-        
-        """
+                """
         Dialog to define current geotuileur import data
 
         Args:
             parent: parent QObject
         """
+        
+        super().__init__(parent)
+        
+
         uic.loadUi(os.path.join(os.path.dirname(__file__), "wzd_import.ui"), self)
 
         # # To avoid some characters 
-        rx=QtCore.QRegExp("[a-z-A-Z-0-9-_]+")
+        
+        rx = QtCore.QRegExp("[a-z-A-Z-0-9-_]+")
         validator = QtGui.QRegExpValidator(rx)
         self.lne_data.setValidator(validator)
 
-        self.phb_ok.clicked.connect(self.file_path)
+        self.phb_ok.clicked.connect(self.add_file_path)
 
         self.shortcut_close = QShortcut(QtGui.QKeySequence('Del'), self)
         self.shortcut_close.activated.connect(self.shortcut_del)
-        self.list_filepath=[]
 
     def shortcut_del(self) :
         """
@@ -47,19 +49,19 @@ class WzdImport(QWizard):
         item = self.lvw_import_data.takeItem(row)
         del item       
         
-    def file_path(self):
-        """
-        to accept only a true file path and to show this only once  
-         
-        """
-
-        savepath=self.flw_files_put.filePath()
+    def add_file_path(self):
         
-        for i in range (0,len(self.list_filepath)):
-            if savepath==self.list_filepath[i]:
-                return
+        """ 
+        to accept only a true file path and to show this only once  
+
+        """
+        
+
+        savepath = self.flw_files_put.filePath()
+        
+        items = self.lvw_import_data.findItems( savepath , QtCore.Qt.MatchCaseSensitive)
 
         if QtCore.QFileInfo(savepath).exists() :
-            self.lvw_import_data.addItem(savepath)
-            self.list_filepath.append(savepath)
+            if len(items) == 0:
+                self.lvw_import_data.addItem(savepath)
 

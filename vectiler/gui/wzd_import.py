e7d72c60b42744c40f7181db3c29c36aca297744
Sophie Aubier <sophie.aubier@oslandia.com>
2022-06-15 18:38:16 +0200

    minor fixes

--- vectiler/gui/wzd_import.py
+++ vectiler/gui/wzd_import.py
@@ -2,16 +2,16 @@
 
 import os
 
-# PyQGIS
-
-from qgis.PyQt import uic
-from PyQt5.QtWidgets import QWizard, QShortcut
 from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import QAbstractItemView, QShortcut, QWizard
+from qgis.PyQt import uic
+
+# PyQGIS
 
 
 class WzdImport(QWizard):
-
     def __init__(self, parent=None):
+
         """
         Dialog to define current geotuileur import data
 
@@ -23,36 +23,39 @@ class WzdImport(QWizard):
 
         uic.loadUi(os.path.join(os.path.dirname(__file__), "wzd_import.ui"), self)
 
-        # # To avoid some characters 
+        # # To avoid some characters
 
         rx = QtCore.QRegExp("[a-z-A-Z-0-9-_]+")
         validator = QtGui.QRegExpValidator(rx)
         self.lne_data.setValidator(validator)
 
+        self.lvw_import_data.setSelectionMode(QAbstractItemView.MultiSelection)
+
         self.phb_ok.clicked.connect(self.add_file_path)
 
-        self.shortcut_close = QShortcut(QtGui.QKeySequence('Del'), self)
+        self.shortcut_close = QShortcut(QtGui.QKeySequence("Del"), self)
         self.shortcut_close.activated.connect(self.shortcut_del)
 
     def shortcut_del(self):
-        """
-         To create  shortcut which delete a filepath 
 
         """
+        Create  shortcut which delete a filepath
 
-        row = self.lvw_import_data.currentRow()
-        item = self.lvw_import_data.takeItem(row)
-        del item
+        """
+        for x in self.lvw_import_data.selectedIndexes():
+
+            row = x.row()
+            item = self.lvw_import_data.takeItem(row)
+            del item
 
     def add_file_path(self):
 
-        """ 
-        to accept only a true file path and to show this only once  
+        """
+        Add the file path to the list Widget
 
         """
-
         savepath = self.flw_files_put.filePath()
-
+        print(savepath)
         items = self.lvw_import_data.findItems(savepath, QtCore.Qt.MatchCaseSensitive)
 
         if QtCore.QFileInfo(savepath).exists():

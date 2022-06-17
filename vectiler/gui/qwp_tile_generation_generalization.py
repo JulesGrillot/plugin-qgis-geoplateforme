9ce6957e59c362c9ff5db583280668918a169751
Jean-Marie KERLOCH <jean-marie.kerloch@oslandia.com>
2022-06-17 12:52:59 +0000

    Feature/generalization process wizard

---
+++ vectiler/gui/qwp_tile_generation_generalization.py
@@ -0,0 +1,158 @@
+# standard
+import os
+
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QWizardPage, QRadioButton, QLabel, QWidget
+from qgis.PyQt import uic
+
+from vectiler.__about__ import DIR_PLUGIN_ROOT
+from vectiler.gui.qwp_tile_generation_edition import TileGenerationEditionPageWizard
+
+
+class PixmapLabel(QLabel):
+
+    def __init__(self, parent: QWidget = None):
+        """
+        QLabel implementation for QPixmap display and automatic pixmap rescale
+
+        Args:
+            parent: parent QWidget
+        """
+        super().__init__(parent)
+        self.setMinimumSize(1, 1)
+        self.setScaledContents(False)
+        self._pixmap = None
+
+    def setPixmap(self, pm: QPixmap) -> None:
+        self._pixmap = pm
+        super().setPixmap(pm)
+
+    def heightForWidth(self, width: int) -> int:
+        if self._pixmap is None:
+            return self.height()
+        else:
+            return int(self._pixmap.height() * width / self._pixmap.width())
+
+    def sizeHint(self) -> QSize:
+        w = self.width()
+        return QSize(w, self.heightForWidth(w))
+
+    def scaledPixmap(self) -> QPixmap:
+        return self._pixmap.scaled(self.size(), QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation)
+
+    def resizeEvent(self, a0: QtGui.QResizeEvent) -> None:
+        if self._pixmap is not None:
+            super().setPixmap(self.scaledPixmap())
+
+
+class TileGenerationGeneralizationPageWizard(QWizardPage):
+    def __init__(self, qwp_tile_generation_edition: TileGenerationEditionPageWizard, parent=None):
+        """
+        QWizardPage to define fields for tile generation
+
+        Args:
+            parent: parent QObject
+        """
+
+        super().__init__(parent)
+        self.qwp_tile_generation_edition = qwp_tile_generation_edition
+
+        uic.loadUi(os.path.join(os.path.dirname(__file__), "qwp_tile_generation_generalization.ui"), self)
+
+        self.tippecanoe_options = {
+            'simplify_forms':
+                {'name': 'simplify_forms',
+                 'title': self.tr("Simplification de données hétérogènes"),
+                 'explain': self.tr("Toutes les formes sont simplifiées."),
+                 'value': '-S10'},
+            'keep_nodes':
+                {'name': 'keep_nodes',
+                 'title': self.tr("Simplification de réseau"),
+                 'explain': self.tr("Toutes les formes sont simplifiées et les nœuds du réseau sont conservés."),
+                 'value': '-pn –S15'},
+            'delete_smallest':
+                {'name': 'delete_smallest',
+                 'title': self.tr("Simplification de données linéaires autres"),
+                 'explain': self.tr("Les petits objets sont supprimés."),
+                 'value': '-an –S15'},
+            'keep_cover':
+                {'name': 'keep_cover',
+                 'title': self.tr("Schématisation de données surfaciques"),
+                 'explain': self.tr("Les formes sont simplifiées en conservant une couverture du territoire."),
+                 'value': '-aL –D8 –S15'},
+            'keep_densest_delete_smallest':
+                {'name': 'keep_densest_delete_smallest',
+                 'title': self.tr("Sélection de données surfaciques"),
+                 'explain': self.tr("Les données les plus représentatives sont conservées et les "
+                                    "plus petites supprimées. Ce choix est pertinent si 3 attributs "
+                                    "ou moins sont conservés à l'étape précédente."),
+                 'value': '-ac –aD -an –S15'},
+            'merge_same_attributes_and_simplify':
+                {'name': 'merge_same_attributes_and_simplify',
+                 'title': self.tr("Fusion attributaire de données surfaciques"),
+                 'explain': self.tr(
+                     "Les objets qui ont les mêmes valeurs d’attribut sont fusionnés tout en simplifiant "
+                     "les formes et en supprimant les petites surfaces. "
+                     "Ce choix est pertinent si 3 attributs ou moins sont conservés à l'étape précédente."),
+                 'value': '-ac -an –S10'},
+            'keep_shared_edges':
+                {'name': 'keep_shared_edges',
+                 'title': self.tr("Harmonisation de données surfaciques"),
+                 'explain': self.tr("Les formes sont simplifiées en conservant les limites partagées entre deux "
+                                    "surfaces."),
+                 'value': '-ab –S20'},
+
+        }
+
+        nb_max_col = 3
+        nb_row_for_options = 3
+        minimum_width = 200
+        i = 0
+        for key, option in self.tippecanoe_options.items():
+            # Define column from nb_max column
+            column = i % nb_max_col
+
+            # Define row from nb_max column
+            row = i // nb_max_col * nb_row_for_options
+
+            # Add radiobutton
+            rb_option = QRadioButton(option["title"], self)
+            rb_option.setMinimumWidth(minimum_width)
+            self.tippecanoe_layout.addWidget(rb_option, row, column)
+            row = row + 1
+
+            # Store map of radiobutton
+            option["radiobutton"] = rb_option
+
+            # Add description label
+            desc_label = QLabel(option["explain"], self)
+            desc_label.setWordWrap(True)
+            rb_option.setMinimumWidth(minimum_width)
+            self.tippecanoe_layout.addWidget(desc_label, row, column)
+            row = row + 1
+
+            # Add PixmapLabel for example
+            image_path = DIR_PLUGIN_ROOT / 'resources' / 'images' / 'tippecanoe' / f'{option["name"]}_merged.jpg'
+
+            pixmap = QPixmap(str(image_path))
+            image_label = PixmapLabel(self)
+            image_label.setMinimumWidth(minimum_width)
+            image_label.setMinimumHeight(100)
+            image_label.setPixmap(pixmap)
+            self.tippecanoe_layout.addWidget(image_label, row, column)
+            row = row + 1
+
+            i = i + 1
+
+    def get_tippecanoe_value(self) -> str:
+        """
+        Get selected generalization option tippecanoe value
+
+        Returns: (str) selected tippecanoe value
+
+        """
+        for name, option in self.tippecanoe_options.items():
+            if option['radiobutton'].isChecked():
+                return option["value"]
--- vectiler/gui/wzd_configuration.py
+++ vectiler/gui/wzd_configuration.py
@@ -5,6 +5,7 @@ from qgis.PyQt.QtWidgets import  QWizard
 
 from vectiler.gui.qwp_tile_generation_edition import TileGenerationEditionPageWizard
 from vectiler.gui.qwp_tile_generation_fields_selection import TileGenerationFieldsSelectionPageWizard
+from vectiler.gui.qwp_tile_generation_generalization import TileGenerationGeneralizationPageWizard
 
 
 class WzdConfiguration(QWizard):
@@ -20,5 +21,7 @@ class WzdConfiguration(QWizard):
 
         self.qwp_tile_generation_edition = TileGenerationEditionPageWizard(self)
         self.qwp_tile_generation_fields_selection = TileGenerationFieldsSelectionPageWizard(self.qwp_tile_generation_edition, self)
+        self.qwp_tile_generation_generalization = TileGenerationGeneralizationPageWizard(self.qwp_tile_generation_edition, parent=self)
         self.addPage(self.qwp_tile_generation_edition)
         self.addPage(self.qwp_tile_generation_fields_selection)
+        self.addPage(self.qwp_tile_generation_generalization)

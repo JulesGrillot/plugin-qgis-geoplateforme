a29ef758bfad9c1233d2e7a68a5a251e08e88007
jmkerloch <jean-marie.kerloch@oslandia.com>
2022-08-04 13:56:05 +0200

    connect all available action to slot (message for unavailable actions and add tile creation wizard use)

--- geotuileur/gui/dashboard/wdg_dashboard.py
+++ geotuileur/gui/dashboard/wdg_dashboard.py
@@ -11,6 +11,8 @@ from geotuileur.gui.proxy_model_stored_data import StoredDataProxyModel
 from geotuileur.gui.publication_creation.wzd_publication_creation import (
     PublicationFormCreation,
 )
+from geotuileur.gui.tile_creation.wzd_tile_creation import TileCreationWizard
+from geotuileur.toolbelt import PlgLogger
 
 
 class DashboardWidget(QWidget):
@@ -22,6 +24,7 @@ class DashboardWidget(QWidget):
             parent: parent QWidget
         """
         super().__init__(parent)
+        self.log = PlgLogger().log
 
         uic.loadUi(
             os.path.join(os.path.dirname(__file__), "wdg_dashboard.ui"),
@@ -92,73 +95,178 @@ class DashboardWidget(QWidget):
     def _item_clicked(
         self, index: QModelIndex, proxy_model: StoredDataProxyModel
     ) -> None:
-        if index.column() == self.mdl_stored_data.ACTION_COL:
-            source_index = proxy_model.mapToSource(index)
-            stored_data = self.mdl_stored_data.data(
-                self.mdl_stored_data.index(
-                    source_index.row(), self.mdl_stored_data.NAME_COL
-                ),
-                QtCore.Qt.UserRole,
+        """
+        Launch action for selected table item depending on clicked column
+
+        Args:
+            index: selected index
+            proxy_model: used StoredDataProxyModel
+        """
+        # Get StoredData
+        stored_data = proxy_model.data(
+            proxy_model.index(index.row(), self.mdl_stored_data.NAME_COL),
+            QtCore.Qt.UserRole,
+        )
+        if stored_data:
+            if index.column() == self.mdl_stored_data.ACTION_COL:
+                self._stored_data_main_action(stored_data)
+            elif index.column() == self.mdl_stored_data.DELETE_COL:
+                self._delete(stored_data)
+            elif index.column() == self.mdl_stored_data.REPORT_COL:
+                self._show_report(stored_data)
+            elif index.column() == self.mdl_stored_data.OTHER_ACTIONS_COL:
+                self._stored_data_other_actions(stored_data)
+
+    def _stored_data_main_action(self, stored_data: StoredData):
+        """
+        Execute stored data main action depending on current stored data status and step
+
+        Args:
+            stored_data: (StoredData) stored data
+        """
+        status = StoredDataStatus[stored_data.status]
+        if status == StoredDataStatus.GENERATING:
+            self._show_report(stored_data)
+        elif status == StoredDataStatus.GENERATED:
+            current_step = stored_data.get_current_step()
+            if current_step == StoredDataStep.TILE_GENERATION:
+                self._generate_tile_wizard(stored_data)
+            elif current_step == StoredDataStep.TILE_SAMPLE:
+                self._tile_sample_wizard(stored_data)
+            elif current_step == StoredDataStep.TILE_PUBLICATION:
+                self._publish_wizard(stored_data)
+            elif current_step == StoredDataStep.PUBLISHED:
+                self._view_tile(stored_data)
+        else:
+            self._show_report(stored_data)
+
+    def _stored_data_other_actions(self, stored_data: StoredData):
+        """
+        Display menu for other actions on stored data
+
+        Args:
+            stored_data: (StoredData) stored data
+        """
+        if stored_data.get_current_step() == StoredDataStep.PUBLISHED:
+            menu = QMenu(self)
+
+            replace_action = QAction(self.tr("Replace data"))
+            replace_action.triggered.connect(
+                lambda: self._replace_data_wizard(stored_data)
             )
-            if stored_data:
-                status = StoredDataStatus[stored_data.status]
-                if status == StoredDataStatus.GENERATING:
-                    print("TODO : implement report action")
-                elif status == StoredDataStatus.GENERATED:
-                    current_step = stored_data.get_current_step()
-                    if current_step == StoredDataStep.TILE_GENERATION:
-                        print("TODO : implement tile generation action")
-                    elif current_step == StoredDataStep.TILE_SAMPLE:
-                        print("TODO : implement view sample tile action")
-                    elif current_step == StoredDataStep.TILE_PUBLICATION:
-                        self._publish(stored_data)
-                    elif current_step == StoredDataStep.PUBLISHED:
-                        print("TODO : implement view tile action")
-                else:
-                    print("TODO : implement report action")
+            menu.addAction(replace_action)
 
-        elif index.column() == self.mdl_stored_data.DELETE_COL:
-            print("TODO : implement delete action")
-        elif index.column() == self.mdl_stored_data.REPORT_COL:
-            print("TODO : implement report action")
+            style_action = QAction(self.tr("Manage styles"))
+            style_action.triggered.connect(lambda: self._style_wizard(stored_data))
+            menu.addAction(style_action)
 
-        elif index.column() == self.mdl_stored_data.OTHER_ACTIONS_COL:
-            source_index = proxy_model.mapToSource(index)
-            stored_data = self.mdl_stored_data.data(
-                self.mdl_stored_data.index(
-                    source_index.row(), self.mdl_stored_data.NAME_COL
-                ),
-                QtCore.Qt.UserRole,
+            update_publish_action = QAction(self.tr("Update publication informations"))
+            update_publish_action.triggered.connect(
+                lambda: self._publish_info_update_wizard(stored_data)
             )
-            if stored_data:
-                if stored_data.get_current_step() == StoredDataStep.PUBLISHED:
-                    menu = QMenu(self)
+            menu.addAction(update_publish_action)
 
-                    replace_action = QAction(self.tr("Replace data"))
-                    replace_action.setEnabled(False)
-                    menu.addAction(replace_action)
+            unpublish_action = QAction(self.tr("Unpublish"))
+            unpublish_action.triggered.connect(lambda: self._unpublish(stored_data))
+            menu.addAction(unpublish_action)
 
-                    style_action = QAction(self.tr("Manage styles"))
-                    style_action.setEnabled(False)
-                    menu.addAction(style_action)
+            menu.exec(QCursor.pos())
 
-                    update_publish_action = QAction(
-                        self.tr("Update publication informations")
-                    )
-                    update_publish_action.setEnabled(False)
-                    menu.addAction(update_publish_action)
+    def _delete(self, stored_data: StoredData) -> None:
+        """
+        Delete a stored data
 
-                    unpublish_action = QAction(self.tr("Unpublish"))
-                    unpublish_action.setEnabled(False)
-                    menu.addAction(unpublish_action)
+        Args:
+            stored_data: (StoredData) stored data to delete
+        """
+        self.log("Stored data delete not implemented yet", push=True)
 
-                    menu.exec(QCursor.pos())
+    def _show_report(self, stored_data: StoredData) -> None:
+        """
+        Show report for a stored data
 
-    def _publish(self, stored_data: StoredData) -> None:
+        Args:
+            stored_data: (StoredData) stored data to publish
+        """
+        self.log("Stored data report not implemented yet", push=True)
+
+    def _tile_sample_wizard(self, stored_data: StoredData) -> None:
+        """
+        Show tile sample wizard for a stored data
+
+        Args:
+            stored_data: (StoredData) stored data to view tile sample wizard
+        """
+        self.log("Tile sample wizard not implemented yet", push=True)
+
+    def _generate_tile_wizard(self, stored_data: StoredData) -> None:
+        """
+        Show tile generation wizard for a stored data
+
+        Args:
+            stored_data: (StoredData) stored data to generate tile
+        """
+        publication_wizard = TileCreationWizard(self)
+        publication_wizard.set_datastore_id(stored_data.datastore_id)
+        publication_wizard.set_stored_data_id(stored_data.id)
+        publication_wizard.show()
+
+    def _publish_wizard(self, stored_data: StoredData) -> None:
+        """
+        Show publish wizard for a stored data
+
+        Args:
+            stored_data: (StoredData) stored data to publish
+        """
         publication_wizard = PublicationFormCreation(self)
         publication_wizard.set_datastore_id(stored_data.datastore_id)
         publication_wizard.set_stored_data_id(stored_data.id)
         publication_wizard.show()
+
+    def _view_tile(self, stored_data: StoredData) -> None:
+        """
+        View tile for a stored data
+
+        Args:
+            stored_data: (StoredData) stored data to be viewed
+        """
+        self.log("Tile view not implemented yet", push=True)
+
+    def _replace_data_wizard(self, stored_data: StoredData) -> None:
+        """
+        Show replace data wizard for a stored data
+
+        Args:
+            stored_data: (StoredData) stored data
+        """
+        self.log("Replace data not implemented yet", push=True)
+
+    def _style_wizard(self, stored_data: StoredData) -> None:
+        """
+        Show style wizard for a stored data
+
+        Args:
+            stored_data: (StoredData) stored data
+        """
+        self.log("Tile style management not implemented yet", push=True)
+
+    def _publish_info_update_wizard(self, stored_data: StoredData) -> None:
+        """
+        Show publish information update wizard for a stored data
+
+        Args:
+            stored_data: (StoredData) stored data
+        """
+        self.log("Publish information update not implemented yet", push=True)
+
+    def _unpublish(self, stored_data: StoredData) -> None:
+        """
+        Unpublish a stored data
+
+        Args:
+            stored_data: (StoredData) stored data
+        """
+        self.log("Unpublish not implemented yet", push=True)
 
     def _create_proxy_model(
         self,

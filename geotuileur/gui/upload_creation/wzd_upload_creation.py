05caab38104052be066f24db873f30f434be592b
jmkerloch <jean-marie.kerloch@oslandia.com>
2022-08-17 10:36:19 +0200

    remove back button on last page

--- geotuileur/gui/upload_creation/wzd_upload_creation.py
+++ geotuileur/gui/upload_creation/wzd_upload_creation.py
@@ -26,6 +26,7 @@ class UploadCreationWizard(QWizard):
         self.addPage(self.qwp_upload_edition)
         self.addPage(self.qwp_upload_creation)
         self.setOption(QWizard.NoBackButtonOnStartPage, True)
+        self.setOption(QWizard.NoBackButtonOnLastPage, True)
         self.setOption(QWizard.NoCancelButtonOnLastPage, True)
 
     def set_datastore_id(self, datastore_id: str) -> None:

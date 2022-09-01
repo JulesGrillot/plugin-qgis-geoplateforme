5a31edc42e3592f2336ef07c1495ca4186a83973
Julien Moura <julien.moura@oslandia.com>
2022-09-01 12:35:04 +0200

    Bump tests

--- tests/qgis/test_plg_preferences.py
+++ tests/qgis/test_plg_preferences.py
@@ -37,6 +37,33 @@ class TestPlgPreferences(unittest.TestCase):
         self.assertIsInstance(settings.version, str)
         self.assertEqual(settings.version, __version__)
 
+        # network and authentication
+        self.assertTrue(hasattr(settings, "url_geotuileur"))
+        self.assertIsInstance(settings.url_geotuileur, str)
+        self.assertEqual(
+            settings.url_geotuileur, "https://qlf-portail-gpf-beta.ign.fr/"
+        )
+        self.assertTrue(hasattr(settings, "url_api_entrepot"))
+        self.assertIsInstance(settings.url_api_entrepot, str)
+        self.assertEqual(
+            settings.url_api_entrepot, "https://plage-geotuileur.cegedim.cloud/"
+        )
+        self.assertTrue(hasattr(settings, "url_service_vt"))
+        self.assertIsInstance(settings.url_service_vt, str)
+        self.assertEqual(settings.url_service_vt, "https://qlf-vt-gpf-beta.ign.fr/")
+        self.assertTrue(hasattr(settings, "url_auth"))
+        self.assertIsInstance(settings.url_auth, str)
+        self.assertEqual(settings.url_auth, "https://iam-ign-qa-ext.cegedim.cloud/")
+        self.assertTrue(hasattr(settings, "auth_realm"))
+        self.assertIsInstance(settings.auth_realm, str)
+        self.assertEqual(settings.auth_realm, "demo")
+        self.assertTrue(hasattr(settings, "auth_client_id"))
+        self.assertIsInstance(settings.auth_client_id, str)
+        self.assertEqual(settings.auth_client_id, "geotuileur-qgis-plugin")
+        self.assertTrue(hasattr(settings, "qgis_auth_id"))
+        self.assertIsInstance(settings.qgis_auth_id, str)
+        self.assertEqual(settings.qgis_auth_id, None)
+
 
 # ############################################################################
 # ####### Stand-alone run ########

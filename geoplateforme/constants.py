924df10766aca897cfcfda24207f0cb3cb96f18b
Jean-Marie Kerloch <jean-marie.kerloch@oslandia.com>
2025-05-14 11:15:08 +0200

    feat(external plugin): load action from external plugin - french_locator - gpf_isochrone_isodistance_itineraire

--- geoplateforme/constants.py
+++ geoplateforme/constants.py
@@ -2,3 +2,9 @@
 OAUTH_DECLARED_REDIRECT_PORTS: list[int] = [
     7070,
 ]  # priority order
+
+
+GPF_PLUGIN_LIST: list[str] = [
+    "french_locator_filter",
+    "gpf_isochrone_isodistance_itineraire",
+]

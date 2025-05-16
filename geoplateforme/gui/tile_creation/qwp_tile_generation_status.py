e98e96e6ce63d1fd99ba7ae057d86982a0f2ebdf
Jean-Marie Kerloch <jean-marie.kerloch@oslandia.com>
2025-05-16 16:12:03 +0200

    fix(status check): timer use ms

--- geoplateforme/gui/tile_creation/qwp_tile_generation_status.py
+++ geoplateforme/gui/tile_creation/qwp_tile_generation_status.py
@@ -203,7 +203,7 @@ class TileGenerationStatusPageWizard(QWizardPage):
 
         # Run timer for tile creation check
         self.create_tile_check_timer.start(
-            int(PlgOptionsManager.get_plg_settings().status_check_sleep / 1000.0)
+            int(PlgOptionsManager.get_plg_settings().status_check_sleep * 1000.0)
         )
 
     def _create_tile_finished(self, context, successful, results):

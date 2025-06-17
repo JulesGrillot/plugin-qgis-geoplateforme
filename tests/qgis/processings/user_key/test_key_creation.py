542021f559628fe19c6d10367b3eb52f15d46885
Jean-Marie Kerloch <jean-marie.kerloch@oslandia.com>
2025-06-17 11:55:56 +0200

    feat(user key): add simple test for OAuth2 key creation

--- tests/qgis/processings/user_key/test_key_creation.py
+++ tests/qgis/processings/user_key/test_key_creation.py
@@ -2,6 +2,7 @@ import pytest_httpserver
 
 from geoplateforme.processing.user_key.create_basic_key import CreateBasicKeyAlgorithm
 from geoplateforme.processing.user_key.create_hash_key import CreateHashKeyAlgorithm
+from geoplateforme.processing.user_key.create_oauth_key import CreateOAuthKeyAlgorithm
 from tests.qgis.processings.conftest import run_alg
 
 
@@ -70,3 +71,34 @@ def test_hash_key_creation(
     assert success
     assert result[CreateHashKeyAlgorithm.CREATED_KEY_ID] == created_key_id
     assert result[CreateHashKeyAlgorithm.CREATED_HASH] == created_hash_value
+
+
+def test_oauth_key_creation(
+    data_geopf_srv: pytest_httpserver.HTTPServer,
+):
+    """
+    Check no error is raised when server return valid return code
+
+    Args:
+        data_geopf_srv: pytest fixture to simulate server and mock plugin settings
+    """
+
+    created_key_id = "key_id"
+
+    # Respond valid return code
+    data_geopf_srv.expect_oneshot_request(
+        "/api/users/me/keys", method="POST"
+    ).respond_with_json(
+        {
+            "name": "name",
+            "type": "OAUTH2",
+            "_id": created_key_id,
+        }
+    )
+
+    params = {
+        CreateOAuthKeyAlgorithm.NAME: "name",
+    }
+    result, success = run_alg(CreateOAuthKeyAlgorithm().name(), params)
+    assert success
+    assert result[CreateOAuthKeyAlgorithm.CREATED_KEY_ID] == created_key_id
